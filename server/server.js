e1f1a79d51558d02f71f29879f92c50fd543dc36
voon220 <peter220@gmail.com>
2022-07-16 15:36:41 +0800

    6th attempt

--- server/server.js
+++ server/server.js
@@ -38,6 +38,15 @@ app.use((err,req,res,next)=>{
     handleError(err,res)
 })
 
+// app.use(express.static('client/build'));
+// if(process.env.NODE_ENV === 'production'){
+//     const path = require('path');
+
+//     app.get('/*',(req,res)=>{
+//         res.sendFile(path.resolve(__dirname,'../client','build','index.html'))
+//     })
+// }
+
 app.use(express.static('client/build'));
 if(process.env.NODE_ENV === 'production'){
     const path = require('path');
